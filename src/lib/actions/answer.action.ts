ed22f1bd497e1197b4f9e158352e08b65119570b
Shlok Prajapati <shlokprajapati2003@gmail.com>
2024-07-12 19:20:58 +0530

    commented answer form

--- src/lib/actions/answer.action.ts
+++ src/lib/actions/answer.action.ts
@@ -1,50 +1,50 @@
-"use server";
+// "use server";
 
-import { revalidatePath } from "next/cache";
-import { connectToDatabase } from "@/lib/mongoose";
+// import { revalidatePath } from "next/cache";
+// import { connectToDatabase } from "@/lib/mongoose";
 
-import Question from "@/database/question.model";
-import Answer, { IAnswer } from "@/database/answer.model";
+// import Question from "@/database/question.model";
+// import Answer, { IAnswer } from "@/database/answer.model";
 
-import { CreateAnswerParams, GetAnswersParams } from "./shared.types";
+// import { CreateAnswerParams, GetAnswersParams } from "./shared.types";
 
-export async function createAnswer(params: CreateAnswerParams) {
-    try {
-        connectToDatabase();
+// export async function createAnswer(params: CreateAnswerParams) {
+//     try {
+//         connectToDatabase();
 
-        const { content, author, question, path } = params;
+//         const { content, author, question, path } = params;
 
-        const answer: IAnswer = new Answer({
-            content,
-            author,
-            question,
-        });
+//         const answer: IAnswer = new Answer({
+//             content,
+//             author,
+//             question,
+//         });
 
-        // Add the answer to the question answer array
-        await Question.findByIdAndUpdate(question, {
-            $push: { answers: answer._id },
-        });
+//         // Add the answer to the question answer array
+//         await Question.findByIdAndUpdate(question, {
+//             $push: { answers: answer._id },
+//         });
 
-        // TODO: Add interactions...
+//         // TODO: Add interactions...
 
-        revalidatePath(path);
-    } catch (error) {
-        console.error(`❌ ${error} ❌`);
-        throw error;
-    }
-}
-export async function getAnswers(params: GetAnswersParams) {
-    try {
-        connectToDatabase();
-        const { questionId } = params;
+//         revalidatePath(path);
+//     } catch (error) {
+//         console.error(`❌ ${error} ❌`);
+//         throw error;
+//     }
+// }
+// export async function getAnswers(params: GetAnswersParams) {
+//     try {
+//         connectToDatabase();
+//         const { questionId } = params;
 
-        const answers = await Answer.find({ question: questionId })
-            .populate("author", "_id clerkId name picture")
-            .sort({ createdAt: -1 });
+//         const answers = await Answer.find({ question: questionId })
+//             .populate("author", "_id clerkId name picture")
+//             .sort({ createdAt: -1 });
 
-        return { answers };
-    } catch (error) {
-        console.error(`❌ ${error} ❌`);
-        throw error;
-    }
-}
+//         return { answers };
+//     } catch (error) {
+//         console.error(`❌ ${error} ❌`);
+//         throw error;
+//     }
+// }
